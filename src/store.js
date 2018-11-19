645a619a6d0cecc389cfa3286704264de9badbd2
Lee Shei Pin <shei.pin@toccostudios.com>
2018-11-20 01:36:40 +0800

    adds vuex mutations, add vuex get all todo action

--- src/store.js
+++ src/store.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getAllTodos } from './apiHelper.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todoList: [],
+    isLoading: false
   },
   mutations: {
-
+    SET_TODOLIST(state, todoList){
+      state.todoList = todoList;
+    },
+    ADD_TODO(state, todo){
+      state.todoList.push(todo);
+    },
+    REMOVE_TODO(state, id){
+      const index = state.todoList.findIndex(x => x.id  === id);
+      state.todoList.splice(index, 1);
+    },
+    UPDATE_TODO(state, todo){
+      const index = state.todoList.findIndex(x => x.id  === todo.id);
+      state.todoList[index] = todo;
+      state.todoList = [...state.todoList]; // allow vue to detect change
+    }
   },
   actions: {
-
+    getTodoList({commit}){
+      getAllTodos().then(json => {
+        commit("SET_TODOLIST", json);
+      })
+    }
   }
 })
