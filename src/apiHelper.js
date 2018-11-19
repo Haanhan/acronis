38f9a8ca6febadbe4f081ac204b903123c63b3e0
Lee Shei Pin <shei.pin@toccostudios.com>
2018-11-20 01:35:44 +0800

    add axios api helper.

---
+++ src/apiHelper.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+const api = "http://localhost:8000/api";
+
+
+export const getAllTodos = () => {
+    return axios.get(`${api}/todos`)
+        .then(response => response.data)
+}
+
+export const getTodo = id => {
+    return axios.get(`${api}/todo/${id}`)
+        .then(response => response.data)
+}
+
+export const updateTodo = todo => {
+    return axios.put(`${api}/todo`, todo)
+        .then(response => response.data)
+}
+
+export const deleteTodo = id => {
+    return axios.delete(`${api}/todo/id`)
+        .then(response => response.data)
+}
+
+export const insertTodo = todo => {
+    return axios.post(`${api}/todo`, todo)
+        .then(response => response.data)
+}
