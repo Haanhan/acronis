9255f0d812cfb34caf10ba64101ffb8b11cd0d95
Lee Shei Pin <shei.pin@toccostudios.com>
2018-11-22 00:13:14 +0800

    Adds input bar

---
+++ src/components/InputBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="inputbar">
+        <el-input class="inputbar-text" :placeholder="placeholder" :value="inputValue">
+        </el-input>
+        <el-button slot="append" @click="btnClick" type="primary">Add</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        placeholder: { type: String }
+    },
+    data(){
+        return {
+            inputValue: ""
+        }
+    },
+    methods:{
+        btnClick(){
+            this.inputValue = "";
+            this.$emit("btn-click", this.inputValue)
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .inputbar{
+        display:flex;
+    }
+    .inputbar-text{
+        margin-right: 10px;
+    }
+</style>
