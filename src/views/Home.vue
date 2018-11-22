7bb436c6e6d29d6a7e3f6f891b02a91318788c47
Lee Shei Pin <shei.pin@toccostudios.com>
2018-11-22 18:48:17 +0800

    Removed el-col from Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,59 @@
 <template>
   <div class="home">
+
+    <h1>To-do App</h1>
           
-    <el-row type="flex" justify="space-around">
-        <el-col :lg="14">
-          
-          <h1>To-do App</h1>
-          
-          <el-card body-style="padding:0">
+    <el-card body-style="padding:0">
 
-            <input-bar slot="header" 
-              @btn-click="addTodo"
-              btn-text="Add"
-              placeholder="What do you need to do?"></input-bar>
+      <input-bar slot="header" 
+        @btn-click="addTodo"
+        btn-text="Add"
+        placeholder="What do you need to do?"></input-bar>
 
-            <el-table :show-header="false" :data="todoList" v-loading="isLoading">
+      <el-table :show-header="false" :data="todoList" v-loading="isLoading">
 
-              <div slot="empty">
-                Nothing to do yet. Add to get started!
-              </div>
+        <div slot="empty">
+          Nothing to do yet. Add to get started!
+        </div>
 
-              <el-table-column :width="40" align="center">
-                <template slot-scope="{row}">
-                  <checkbox :done="row.done" 
-                    :isLoading="row.isPending"
-                    @click.native="setDone(row)"></checkbox>
-                </template>
-              </el-table-column>
+        <el-table-column :width="40" align="center">
+          <template slot-scope="{row}">
+            <checkbox :done="row.done" 
+              :isLoading="row.isPending"
+              @click.native="setDone(row)"></checkbox>
+          </template>
+        </el-table-column>
 
-              <el-table-column>
-                <div slot-scope="{row: {text, done}}" :class="{'done-text': done}">
-                  {{text}}
-                </div>
-              </el-table-column>
+        <el-table-column>
+          <div slot-scope="{row: {text, done}}" :class="{'done-text': done}">
+            {{text}}
+          </div>
+        </el-table-column>
 
-              <el-table-column align="right" width="90">
-                <template slot-scope="{row: {id}}"> 
+        <el-table-column align="right" width="90">
+          <template slot-scope="{row: {id}}"> 
 
-                  <el-button circle
-                    @click="goToEdit(id)"
-                    size="mini" 
-                    icon="el-icon-edit">
-                  </el-button>
+            <el-button circle
+              @click="goToEdit(id)"
+              size="mini" 
+              icon="el-icon-edit">
+            </el-button>
 
-                  <el-button circle
-                    @click="deleteTodo(id)"
-                    size="mini" 
-                    icon="el-icon-delete"
-                    type="danger">
-                  </el-button>
+            <el-button circle
+              @click="deleteTodo(id)"
+              size="mini" 
+              icon="el-icon-delete"
+              type="danger">
+            </el-button>
 
-                </template>
-              </el-table-column>
-              
+          </template>
+        </el-table-column>
+        
 
-            </el-table>
+      </el-table>
 
-          </el-card>
+    </el-card>
 
-        </el-col>
-    </el-row>
 
   </div>
 </template>
